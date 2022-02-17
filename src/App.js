123bc59d2994bc1098d4057b4a6e5a6de34a6dd2
ssugu42 <ssugu.4422@gmail.com>
2022-02-17 20:27:27 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,24 +3,62 @@ import './style.css';
 import { createStore } from 'redux';
 
 const myStoreData = {
-    name: 'Sugumar',
-    age: '24',
-    Status: 'Single',
+  theme: 'White',
+};
+
+const redBg = function () {
+  return {
+    type: 'red',
+  };
+};
+
+const blueBg = function () {
+  return {
+    type: 'blue',
+  };
+};
+
+const greenGb = function () {
+  return {
+    type: 'green',
+  };
+};
+
+const myReducer = function (state = myStoreData, action) {
+  console.log(action.type);
+  switch (action.type) {
+    case 'red':
+      state.theme = 'red';
+      break;
+    case 'blue':
+      state.theme = 'blue';
+      break;
+    case 'green':
+      state.theme = 'green';
+      break;
   }
 
-const myReducer = function (state = myStoreData) {
   return state;
 };
 
 const store = createStore(myReducer);
+store.subscribe(()=>{
+  const backGround = {
+    background: store.getState().theme,
+  };
+})
+console.log(store.getState().theme);
 
-console.log(store.getState());
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>{store.getState().name}</p>
+    <div style={backGround}>
+      <h1>Hello Sugumar!</h1>
+      <p>Welcome to the React course</p>
+      <label htmlFor="btn">Click the buttons to change backgroun:</label>
+      <button onClick={() => store.dispatch(redBg())}>Red</button>
+      <button onClick={() => store.dispatch(blueBg())}>Blue</button>
+      <button onClick={() => store.dispatch(greenGb())}>Green</button>
     </div>
   );
 }
